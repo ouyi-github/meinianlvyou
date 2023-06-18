205dd3d1deead69314ea3feaf4c0527a21b8563e
ouyi <2412632321@qq.com>
2023-06-18 10:46:39 +0800

    meinian_common 测试代码注销

--- meinian_parent/meinian_common/src/test/java/com/atguigu/test/TestQiniu.java
+++ meinian_parent/meinian_common/src/test/java/com/atguigu/test/TestQiniu.java
@@ -20,61 +20,61 @@ import org.junit.Test;
  */
 public class TestQiniu {
 
-    // 上传本地文件
-    @Test
-    public void uploadFile(){
-        //构造一个带指定Zone对象的配置类
-        Configuration cfg = new Configuration(Zone.zone2());//华南
-        //...其他参数参考类注释
-        UploadManager uploadManager = new UploadManager(cfg);
-        //...生成上传凭证，然后准备上传
-        String accessKey = "6wXsRKQW6dprHho6hIOwM7oCY_MITYrhTNKuJzfi";
-        String secretKey = "xEocjgAGkTts3msIWmcL16J6BfMwomfmP5c84zcX";
-        String bucket = "meinian102101";
-        //如果是Windows情况下，格式是 D:\\qiniu\\test.png，可支持中文
-        String localFilePath = "D:/temp/90/gxt97.png";
-        //默认不指定key的情况下，以文件内容的hash值作为文件名
-        String key = null; //自定义文件名称
-        Auth auth = Auth.create(accessKey, secretKey);
-        String upToken = auth.uploadToken(bucket);
-        try {
-            Response response = uploadManager.put(localFilePath, key, upToken);
-            //解析上传成功的结果
-            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
-            System.out.println(putRet.key);// FspfyEyKfuHZ0kcbXRIc5T9YhCax
-            System.out.println(putRet.hash);// FspfyEyKfuHZ0kcbXRIc5T9YhCax
-        } catch (QiniuException ex) {
-            Response r = ex.response;
-            System.err.println(r.toString());
-            try {
-                System.err.println(r.bodyString());
-            } catch (QiniuException ex2) {
-                //ignore
-            }
-        }
-    }
-
-
-
-    // 删除空间中的文件
-    @Test
-    public void deleteFile(){
-        //构造一个带指定Zone对象的配置类
-        Configuration cfg = new Configuration(Zone.zone2());
-        //...其他参数参考类注释
-        String accessKey = "6wXsRKQW6dprHho6hIOwM7oCY_MITYrhTNKuJzfi";
-        String secretKey = "xEocjgAGkTts3msIWmcL16J6BfMwomfmP5c84zcX";
-        String bucket = "meinian102101";
-        String key = "FrbM6qd5TnYqys1EnGkmU2JauTFk";//文件名称
-        Auth auth = Auth.create(accessKey, secretKey);
-        BucketManager bucketManager = new BucketManager(auth, cfg);
-        try {
-            bucketManager.delete(bucket, key);
-        } catch (QiniuException ex) {
-            //如果遇到异常，说明删除失败
-            System.err.println(ex.code());
-            System.err.println(ex.response.toString());
-        }
-    }
+//    // 上传本地文件
+//    @Test
+//    public void uploadFile(){
+//        //构造一个带指定Zone对象的配置类
+//        Configuration cfg = new Configuration(Zone.zone2());//华南
+//        //...其他参数参考类注释
+//        UploadManager uploadManager = new UploadManager(cfg);
+//        //...生成上传凭证，然后准备上传
+//        String accessKey = "6wXsRKQW6dprHho6hIOwM7oCY_MITYrhTNKuJzfi";
+//        String secretKey = "xEocjgAGkTts3msIWmcL16J6BfMwomfmP5c84zcX";
+//        String bucket = "meinian102101";
+//        //如果是Windows情况下，格式是 D:\\qiniu\\test.png，可支持中文
+//        String localFilePath = "D:/temp/90/gxt97.png";
+//        //默认不指定key的情况下，以文件内容的hash值作为文件名
+//        String key = null; //自定义文件名称
+//        Auth auth = Auth.create(accessKey, secretKey);
+//        String upToken = auth.uploadToken(bucket);
+//        try {
+//            Response response = uploadManager.put(localFilePath, key, upToken);
+//            //解析上传成功的结果
+//            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+//            System.out.println(putRet.key);// FspfyEyKfuHZ0kcbXRIc5T9YhCax
+//            System.out.println(putRet.hash);// FspfyEyKfuHZ0kcbXRIc5T9YhCax
+//        } catch (QiniuException ex) {
+//            Response r = ex.response;
+//            System.err.println(r.toString());
+//            try {
+//                System.err.println(r.bodyString());
+//            } catch (QiniuException ex2) {
+//                //ignore
+//            }
+//        }
+//    }
+//
+//
+//
+//    // 删除空间中的文件
+//    @Test
+//    public void deleteFile(){
+//        //构造一个带指定Zone对象的配置类
+//        Configuration cfg = new Configuration(Zone.zone2());
+//        //...其他参数参考类注释
+//        String accessKey = "6wXsRKQW6dprHho6hIOwM7oCY_MITYrhTNKuJzfi";
+//        String secretKey = "xEocjgAGkTts3msIWmcL16J6BfMwomfmP5c84zcX";
+//        String bucket = "meinian102101";
+//        String key = "FrbM6qd5TnYqys1EnGkmU2JauTFk";//文件名称
+//        Auth auth = Auth.create(accessKey, secretKey);
+//        BucketManager bucketManager = new BucketManager(auth, cfg);
+//        try {
+//            bucketManager.delete(bucket, key);
+//        } catch (QiniuException ex) {
+//            //如果遇到异常，说明删除失败
+//            System.err.println(ex.code());
+//            System.err.println(ex.response.toString());
+//        }
+//    }
 
 }
